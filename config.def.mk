16b71a92d658fd9a9b51ef5cf971e3f2e2ea5615
Alvarito050506 <donfrutosgomez@gmail.com>
2022-01-19 10:15:38 -0300

    Remove broken Elementary Dockerfile.

--- config.def.mk
+++ config.def.mk
@@ -1,8 +1,6 @@
 PLATFORMS?=linux/arm64/v8,linux/arm64/v7,linux/amd64
-DISTROS?=alpine debian ubuntu elementary
+DISTROS?=alpine debian ubuntu
 NAMESPACE?=valalang/vala
 TAG_alpine?=latest
 TAG_debian?=stable-slim
 TAG_ubuntu?=latest
-TAG_elementary?=stable
-NS_elementary?=elementary/docker
